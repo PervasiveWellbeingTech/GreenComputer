4184659524aa1e830b9ac49519536a18402ff8f3
Peter Washington <peter.y.washington@gmail.com>
2018-10-17 18:26:58 -0700

    Refactor variable names to make sense in terms of signal processing

--- nshift/main.m
+++ nshift/main.m
@@ -21,31 +21,40 @@ uint64_t time_to_wait_noticeable = 5 * NANOS_PER_SEC;
 static mach_timebase_info_data_t timebase_info;
 
 // PARAMETERS
-float DELTA = 0.01;
-float LOW = 0.5;
-float HIGH = 0.6;
-uint64_t DELAY_SECONDS = 0.0155 * NANOS_PER_SEC;
+float COLOR_DELTA = 0.01;
+float LOW_R = 0.5;
+float HIGH_R = 0.6;
+float LOW_G = 0.5;
+float HIGH_G = 0.6;
+float LOW_B = 0.5;
+float HIGH_B = 0.6;
+uint64_t TIMESTEP = 0.0155 * NANOS_PER_SEC;
 
 /*
  * TODO:
  *
- * - Add parameter to set the period (time to get from LOW back to LOW again)
- * - Basically, parameter to set the frequency
- * - For 40Hz, the period is 0.025
- * - Know the limits of the LED lights
- * - Calibration phase to modify the gamma values as the thing is running.
- * - Make adaptive system where the human perception wavelength matches the wave of the
+ * ---------------------------------------------------------------------------------
+ * ( ) = TODO
+ * ( ) = DONE
+ * ---------------------------------------------------------------------------------
+ *
+ * ( ) Add parameter to set the period (time to get from LOW back to LOW again)
+ * ( ) Basically, parameter to set the frequency
+ * (-) For 40Hz, the period is 0.025
+ * ( ) Know the limits of the LED lights
+ * ( ) Calibration phase to modify the gamma values as the thing is running.
+ * ( ) Make adaptive system where the human perception wavelength matches the wave of the
  *     oscillations put out by the program.
- * - Make code so that it is really easy to tweak all the parameters at very fine grained in order
+ * ( ) Make code so that it is really easy to tweak all the parameters at very fine grained in order
  *     to get people's custom wavelengths.
- * - Figure out how to get baseline RGB. Look for a function for getting Gamma display values.
- * - Detect most common "color" value on the screen, and set that to baseline color of the screen.
- * - This most common value will continuously update.
- * - Add ability to have a delay between oscillations. Perhaps another "delay" parameters.
- * - And also a parameter for how long the wave should last when it reaches the top of
+ * ( ) Figure out how to get baseline RGB. Look for a function for getting Gamma display values.
+ * ( ) Detect most common "color" value on the screen, and set that to baseline color of the screen.
+ * ( ) This most common value will continuously update.
+ * ( ) Add ability to have a delay between oscillations. Perhaps another "delay" parameters.
+ * ( ) And also a parameter for how long the wave should last when it reaches the top of
  *      the wave as well.
- * - Refactor variables to make sense in terms of signal processing / waves.
- * - T = 1 / f
+ * (-) Refactor variables to make sense in terms of signal processing / waves.
+ * (-) T = 1 / f
  */
 
 static uint64_t abs_to_nanos(uint64_t abs) {
@@ -61,13 +70,13 @@ void changeColorRecursive(uint64_t n, bool i, float l) {
     bool is_increasing = i;
     float level = l;
     
-    if (is_increasing) level += DELTA;
-    else level -= DELTA;
+    if (is_increasing) level += COLOR_DELTA;
+    else level -= COLOR_DELTA;
 
-    if (level > HIGH) is_increasing = false;
-    if (level < LOW) is_increasing = true;
+    if (level > HIGH_B) is_increasing = false;
+    if (level < LOW_B) is_increasing = true;
     
-    uint64_t next_time_to_schedule = mach_absolute_time() + DELAY_SECONDS;
+    uint64_t next_time_to_schedule = mach_absolute_time() + TIMESTEP;
     
     [MacGammaController setGammaWithRed:0.5 green:0.5 blue:level ];
     
